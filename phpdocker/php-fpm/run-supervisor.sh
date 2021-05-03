9ea3771d22d5e80568f56711a83fdb67a64ed989
Armen Marabyan <armen.marabyan1@gmail.com>
2021-05-03 18:24:21 +0400

    f

---
+++ phpdocker/php-fpm/run-supervisor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FILE=/var/www/html/storage/logs/worker.log
+#FILE=/var/www/html/menu2/menu.am-restful-api/storage/test.txt
+#if [ ! -f "$FILE" ]; then
+#  touch $FILE
+#fi
+#service supervisor stop
+#/usr/bin/supervisord
+php artisan horizon
